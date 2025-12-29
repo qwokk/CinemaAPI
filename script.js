d586f5b986a6fbe0563caa1361691e8fb0cf6cd3
Assylbek <awoksorpa4ik@gmail.com>
2025-12-29 16:04:21 +0500

    feat: add pagination, scroll-to-top button, and movie details click

--- script.js
+++ script.js
@@ -28,3 +28,83 @@ movieInput.addEventListener('keypress', (e) => {
         fetchMovies()
     }
 });
+
+closeModal.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+prevPageBtn.addEventListener('click', () => {
+    if (currentPage > 1) {
+    currentPage--;
+    fetchMovies();
+    scrollToTop(); 
+}
+});
+
+nextPageBtn.addEventListener('click', () => {
+    currentPage++;
+    fetchMovies();
+    scrollToTop();
+});
+
+window.addEventListener('scroll', () => {
+    if (document.documentElement.scrollTop > 200) {
+        scrollTopBtn.style.display = 'block';
+    } else {
+        scrollTopBtn.style.display = 'none';
+    }
+});
+
+scrollTopBtn.addEventListener('click', () => {
+    scrollToTop();
+})
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth'});
+}
+
+async function fetchMovies() {
+    currentQuery = movieInput.value.trim();
+    if (!currentQuery) {
+        movieContainer.innerHTML = '<p>Please enter a movie name </p>';
+        pagination.style.display = 'none';
+        return;
+    }
+
+    movieContainer.innerHTML = '<p>Loading ...</p>';
+
+    try{
+        const res = await fetch(
+             `https://www.omdbapi.com/?s=${currentQuery}&page=${currentPage}&apikey=${API_KEY}`
+        );
+        const data = await res.json();
+
+        if (data.Response == 'False') {
+            movieContainer.innerHTML = `<p>${data.Error}</p>`;
+            pagination.style.display = 'none';
+            return;
+        }
+        
+        movieContainer.innerHTML = data.Search.map((movie) => `
+        <div class="movie-card" onclick="showDetails('${movie.imdbID}')">
+            <img src="${
+                movie.Poster !== 'N/A'
+                    ? movie.Poster
+                    : 'https://via.placeholder.com/220x330'
+            }" alt="${movie.Title}">
+            <h2>${movie.Title}</h2>
+            <p>${movie.Year}</p>
+      </div>
+        `
+    ).join('');
+
+    pagination.style.display = 'flex';
+    pageInfo.textContent = `Page ${currentPage}`;
+    prevPageBtn.disabled = currentPage == 1;
+    nextPageBtn.disabled = data.Search.length < 10;
+    } catch(e) {
+
+    }
+    
+}
+
